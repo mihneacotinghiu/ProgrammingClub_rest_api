359eef32029d61c068d85b0eb36b26db04086ec7
Andrei CUIBUS <acuibus@pentalog.com>
2023-01-03 09:47:26 +0200

    Added functions to the codesnippetService Interface

--- ProgrammingClub/Services/ICodeSnippetsService.cs
+++ ProgrammingClub/Services/ICodeSnippetsService.cs
@@ -12,6 +12,11 @@ namespace ProgrammingClub.Services
         public Task<bool> DeleteCodeSnippetAsync(Guid id);
         public Task<CodeSnippet?> UpdateCodeSnippetAsync(Guid id, CodeSnippet codeSnippet);
         public Task<CodeSnippet?> PartiallyUpdateCodeSnippetAsync(Guid id, CodeSnippet codeSnippet);
+        public Task<bool> CodeSnippetExistByIdAsync(Guid? id);
+        public Task CodeSnippetIsValid(CodeSnippet codeSnippet);
+        public Task CheckCodeSnippetExistance(Guid? id);
+        public Task CheckMemberExistance(Guid? id);
+
 
     }
 }
