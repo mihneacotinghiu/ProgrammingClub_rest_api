a813de2e24ba87035fe5e00e190b764aab7343be
fgnandt@pentalog.com <fgnandt@pentalog.com>
2023-01-13 11:03:24 +0200

    unit tests

--- UnitTests/MembersTest/MembersServiceTest.cs
+++ UnitTests/MembersTest/MembersServiceTest.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTests.Models.Builders;
 
 namespace UnitTests.MembersTest
 {
@@ -22,7 +23,7 @@ namespace UnitTests.MembersTest
 
         public MembersServiceTest()
         {
-            _contextInMemory = GetDatabaseContext();
+            _contextInMemory = Helpers.DBContextHelper.GetDatabaseContext();
             _mockMapper = new Mock<IMapper>();
 
             _service = new MembersService(_contextInMemory, _mockMapper.Object);
@@ -42,34 +43,62 @@ namespace UnitTests.MembersTest
             var testMember = await Helpers.DBContextHelper.AddTestMember(_contextInMemory);
             Assert.NotNull(testMember);
 
-            var result = await _service.DeleteMember(testMember.IdMember.Value);
+            var memberId = testMember.IdMember.Value;
+            var result = await _service.DeleteMember(memberId);
             Assert.True(result);
+            Assert.False(await _contextInMemory.Members.AnyAsync( x=> x.IdMember == memberId));
         }
 
         [Fact]
         public async Task GetMemberById_MemberExist_ReturnMemberAsync()
         {
+            var testMember = new MemberBuilder().With(x => { x.Name = "TestName"; }).Build();
+            testMember = await Helpers.DBContextHelper.AddTestMember(_contextInMemory, testMember);
+            Assert.NotNull(testMember);
+
+            var memberId = testMember.IdMember.Value;
+            var result = await _service.GetMemberById(memberId);
+            Assert.NotNull(result);
+            var memberFromDatabase = await _contextInMemory.Members.FirstOrDefaultAsync(x => x.IdMember == memberId);
+            Assert.Equal(testMember.Name, memberFromDatabase.Name);
+            Assert.Equal(testMember.Description, memberFromDatabase.Description);
+            Assert.Equal(testMember.Title, memberFromDatabase.Title);
+            Assert.Equal(testMember.Position, memberFromDatabase.Position);
+            Assert.Equal(testMember.Resume, memberFromDatabase.Resume);
+        }
+
+        [Fact]
+        public async Task UpdatePartiallyMember_MemberExistEmptyName_MemberNameWasNotChangedAsync()
+        {
             var testMember = await Helpers.DBContextHelper.AddTestMember(_contextInMemory);
             Assert.NotNull(testMember);
 
-            var result = await _service.GetMemberById(testMember.IdMember.Value);
+            var memberId = testMember.IdMember.Value;
+            var memberData = new MemberBuilder().With(x => { x.Name = ""; }).Build();
+            var result = await _service.UpdatePartiallyMember(memberId, memberData);
             Assert.NotNull(result);
-            Assert.Equal(testMember.Name, result.Name);
-            Assert.Equal(testMember.Description, result.Description);
-            Assert.Equal(testMember.Title, result.Title);
-            Assert.Equal(testMember.Position, result.Position);
-            Assert.Equal(testMember.Resume, result.Resume);
+
+            var memberAfterUpdate = await _contextInMemory.Members.FirstOrDefaultAsync(x => x.IdMember == memberId);
+            Assert.Equal(testMember.Name, memberAfterUpdate.Name);
         }
 
-        private ProgrammingClubDataContext GetDatabaseContext()
+        [Fact]
+        public async Task UpdatePartiallyMember_MemberExistNoValidationError_MemberUpdatedAsync()
         {
-            var options = new DbContextOptionsBuilder<ProgrammingClubDataContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-                .Options;
-            var databaseContext = new ProgrammingClubDataContext(options);
-            databaseContext.Database.EnsureCreated();
-            return databaseContext;
+            var testMember = await Helpers.DBContextHelper.AddTestMember(_contextInMemory);
+            Assert.NotNull(testMember);
+
+            var memberId = testMember.IdMember.Value;
+            var memberToUpdate = new MemberBuilder().With(x => { x.Name = "NameX"; x.Resume = "ResumeX"; }).Build();
+            var result = await _service.UpdatePartiallyMember(memberId, memberToUpdate);
+            Assert.NotNull(result);
+
+            var memberFromDatabase = await _contextInMemory.Members.FirstOrDefaultAsync(x => x.IdMember == memberId);
+            Assert.Equal(memberFromDatabase.Name, memberToUpdate.Name);
+            Assert.Equal(memberFromDatabase.Description, memberToUpdate.Description);
+            Assert.Equal(memberFromDatabase.Title, memberToUpdate.Title);
+            Assert.Equal(memberFromDatabase.Position, memberToUpdate.Position);
+            Assert.Equal(memberFromDatabase.Resume, memberToUpdate.Resume);
         }
     }
 }
